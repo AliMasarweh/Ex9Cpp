586c2755e68d0cfaff969df57c569812b4e92910
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 21:27:52 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,12 +1,12 @@
 #include "TestCase.hpp"
 
-TestCase::TestCase(string name, ostream& c) : errorPrinter(c.rdbuf()) {
-	nameOfTest = name;
-	total = passed = failed = 0;
+TestCase::TestCase(string name, ostream& c) : c(c.rdbuf()) {
+            _name = name;
+            total = passed = failed = 0;
 }
 
-void TestCase::print() {
-
-	cout << nameOfTest << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
-	cout << "---" << endl;
+void TestCase::print(){
+    
+    c << _name << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
+    c << "---" << endl;
 }
