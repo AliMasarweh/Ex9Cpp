e3957c678e45f37967c6a541428919e4521d89df
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 21:27:30 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -7,86 +7,86 @@
 using namespace std;
 
 
-class TestCase {
-
-private:
-	int total, passed, failed;
-	string nameOfTest;
-	ostream errorPrinter;
-
-
-
-public:
-
-	TestCase(string name, ostream& errorPrinter);
-	void print();
-
-	template <typename T> TestCase& check_equal(T a, T b) {
-
-		total++;
-
-		if (a == b) {
-			passed++;
-		}
-		else {
-			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << a << " should equal " << b << "!" << endl;
-			failed++;
-		}
-
-		return *this;
-	}
-
-	template <typename T> TestCase& check_different(T a, T b) {
-
-		total++;
-
-		if (a != b) {
-			passed++;
-		}
-		else {
-			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << a << " should not equal " << b << "!" << endl;
-			failed++;
-		}
-
-		return *this;
-	}
-
-	/*
-	we need another tamplate variable in orfer to execute differents options on him.
-
-	*/
-	template<typename Fun, typename T> TestCase& check_function(Fun func, const T a, const int b) {
-
-		total++;
-		int res = (*func)(a);
-
-		if (res == b) {
-			passed++;
-		}
-		else {
-			errorPrinter << nameOfTest << ": Failure in test #" << total << ": Function should return " << b << " but returned " << res << "!" << endl;
-			failed++;
-		}
-
-		return *this;
-	}
-
-
-	template <typename T> TestCase& check_output(T a, string str) {
-
-		total++;
-		ostringstream ss;
-		ss << a;
-
-		if (ss.str() == str) {
-			passed++;
-		}
-		else {
-			errorPrinter << nameOfTest << ": Failure in test #" << total << ": string value should be " << str << " but is " << ss.str() << "!" << endl;
-			failed++;
-		}
-
-		return *this;
-	}
+class TestCase{
+    
+    private:
+        int total, passed, failed;
+        string _name;
+        ostream c;
+        
+        
+        
+    public:
+    
+        TestCase(string name, ostream& c);
+        void print();
+        
+        template <typename T> TestCase& check_equal(T a, T b){
+            
+            total++;
+            
+            if(a == b){
+                passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" <<  total << ": " << a << " should equal " << b << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        template <typename T> TestCase& check_different (T a, T b){
+            
+            total++;
+            
+            if (a != b){
+                passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" << total << ": " << a << " should not equal " << b << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        /*
+        we need another tamplate variable in orfer to execute differents options on him.
+        
+        */
+        template<typename Fun , typename T> TestCase& check_function(Fun func,const T a, const int b){
+            
+            total++;
+            int res = (*func)(a);
+            
+            if(res == b){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": Function should return " << b << " but returned " << res << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        
+        template <typename T> TestCase& check_output(T a,string str){
+            
+            total++;
+            ostringstream ss;
+            ss << a;
+            
+            if(ss.str() == str){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": string value should be " << str << " but is " << ss.str() << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        }
 };
 #endif
