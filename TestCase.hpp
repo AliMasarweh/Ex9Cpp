b22c91f81a024c3a5bbf994fd3c1a2b6fbec8382
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 21:11:35 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -2,24 +2,99 @@
 #ifndef TESTCASE_HPP_
 #define TESTCASE_HPP_
 
-#include <iostream>
 #include <sstream>
-#include <string>
-#include <string.h>
+#include <iostream>
 using namespace std;
 
-class TestCase {
-private:
-	string name;
-	ostream& errorPrinter;
-	int passed , failed, total;
 
-public:
-	TestCase(const string, ostream&);
-	template <typename T> void check_equal(T, T);
-	template <typename T> void check_different(T, T);
-	template <typename T> void check_output(T, string);
-	template <typename T, typename V> void TestCase::check_function(V(*function)(T), T object1, V object2);
-	void print();
+class TestCase{
+    
+    private:
+        int total, passed, failed;
+        string _name;
+        ostream c;
+        
+        
+        
+    public:
+    
+        TestCase(string name, ostream& c);
+        void print();
+        
+        template <typename T> TestCase& check_equal(T a, T b){
+            
+            total++;
+            
+            if(a == b){
+                passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" <<  total << ": " << a << " should equal " << b << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        template <typename T> TestCase& check_different (T a, T b){
+            
+            total++;
+            
+            if (a != b){
+                passed++;
+            } 
+            else{
+                c << _name << ": Failure in test #" << total << ": " << a << " should not equal " << b << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        /*
+        we need another tamplate variable in orfer to execute differents options on him.
+        
+        */
+        template<typename Fun , typename T> TestCase& check_function(Fun func,const T a, const int b){
+            
+            total++;
+            int res = (*func)(a);
+            
+            if(res == b){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": Function should return " << b << " but returned " << res << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        } 
+        
+        
+        template <typename T> TestCase& check_output(T a,string str){
+            
+            total++;
+            ostringstream ss;
+            ss << a;
+            
+            if(ss.str() == str){
+                passed++;
+            } 
+            else{
+               c << _name << ": Failure in test #" << total << ": string value should be " << str << " but is " << ss.str() << "!" << endl;
+                failed++;
+            }
+            
+            return *this;
+        }
+        
+        
+        // TestCase& print(){
+            
+        //     c << _name << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
+        //     c << "---" << endl;
+        //     return *this;
+        // }
 };
 #endif
