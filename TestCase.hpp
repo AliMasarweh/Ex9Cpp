208f3d2bf297cd1394eceb9b32e0b5703d58328a
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 22:04:21 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -13,20 +13,13 @@ private:
 	int total, passed, failed;
 	string nameOfTest;
 	ostream errorPrinter;
-
-
-
 public:
-
 	TestCase(string name, ostream& cerr);
 	TestCase& print();
-
 	//every method of testing returns the object itself , allowing the user to test a several cases in one statement
 
-	template <typename T> TestCase& check_equal(T object1, T object2) {
-
+	template<typename T> TestCase& check_equal(T object1, T object2) {
 		total++;
-
 		if (object1 == object2) {
 			passed++;
 		}
@@ -34,14 +27,11 @@ public:
 			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << object1 << " should equal " << object2 << "!" << endl;
 			failed++;
 		}
-
 		return *this;
 	}
 
-	template <typename T> TestCase& check_different(T object1, T object2) {
-
+	template<typename T> TestCase& check_different(T object1, T object2) {
 		total++;
-
 		if (object1 != object2) {
 			passed++;
 		}
@@ -49,15 +39,12 @@ public:
 			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << object1 << " should not equal " << object2 << "!" << endl;
 			failed++;
 		}
-
 		return *this;
 	}
 
 	template<typename Fun, typename Arg, typename Res> TestCase& check_function(Fun func, const Arg object1, const Res object2) {
-
 		total++;
 		Res res = (*func)(object1);
-
 		if (res == object2) {
 			passed++;
 		}
@@ -65,17 +52,13 @@ public:
 			errorPrinter << nameOfTest << ": Failure in test #" << total << ": Function should return " << object2 << " but returned " << res << "!" << endl;
 			failed++;
 		}
-
 		return *this;
 	}
-
-
-	template <typename T> TestCase& check_output(T object1, string expectedStr) {
-
+	
+	template<typename T> TestCase& check_output(T object1, string expectedStr) {
 		total++;
 		ostringstream ss;
 		ss << object1;
-
 		if (ss.str() == expectedStr) {
 			passed++;
 		}
@@ -83,7 +66,6 @@ public:
 			errorPrinter << nameOfTest << ": Failure in test #" << total << ": string value should be " << expectedStr << " but is " << ss.str() << "!" << endl;
 			failed++;
 		}
-
 		return *this;
 	}
 };
