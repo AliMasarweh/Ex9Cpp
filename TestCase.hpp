ec01e5d5d8ab83681b4a261269ce6fed509dc610
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 21:50:14 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -7,86 +7,82 @@
 using namespace std;
 
 
-class TestCase{
-    
-    private:
-        int total, passed, failed;
-        string _name;
-        ostream c;
-        
-        
-        
-    public:
-    
-        TestCase(string name, ostream& c);
-        void print();
-        
-        template <typename T> TestCase& check_equal(T a, T b){
-            
-            total++;
-            
-            if(a == b){
-                passed++;
-            } 
-            else{
-                c << _name << ": Failure in test #" <<  total << ": " << a << " should equal " << b << "!" << endl;
-                failed++;
-            }
-            
-            return *this;
-        } 
-        
-        template <typename T> TestCase& check_different (T a, T b){
-            
-            total++;
-            
-            if (a != b){
-                passed++;
-            } 
-            else{
-                c << _name << ": Failure in test #" << total << ": " << a << " should not equal " << b << "!" << endl;
-                failed++;
-            }
-            
-            return *this;
-        } 
-        
-        /*
-        we need another tamplate variable in orfer to execute differents options on him.
-        
-        */
-        template<typename Fun , typename T> TestCase& check_function(Fun func,const T a, const int b){
-            
-            total++;
-            int res = (*func)(a);
-            
-            if(res == b){
-                passed++;
-            } 
-            else{
-               c << _name << ": Failure in test #" << total << ": Function should return " << b << " but returned " << res << "!" << endl;
-                failed++;
-            }
-            
-            return *this;
-        } 
-        
-        
-        template <typename T> TestCase& check_output(T a,string str){
-            
-            total++;
-            ostringstream ss;
-            ss << a;
-            
-            if(ss.str() == str){
-                passed++;
-            } 
-            else{
-               c << _name << ": Failure in test #" << total << ": string value should be " << str << " but is " << ss.str() << "!" << endl;
-                failed++;
-            }
-            
-            return *this;
-        }
+class TestCase {
+
+private:
+	int total, passed, failed;
+	string nameOfTest;
+	ostream errorPrinter;
+
+
+
+public:
+
+	TestCase(string name, ostream& cerr);
+	void print();
+
+	template <typename T> TestCase& check_equal(T object1, T object2) {
+
+		total++;
+
+		if (object1 == object2) {
+			passed++;
+		}
+		else {
+			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << object1 << " should equal " << object2 << "!" << endl;
+			failed++;
+		}
+
+		return *this;
+	}
+
+	template <typename T> TestCase& check_different(T object1, T object2) {
+
+		total++;
+
+		if (object1 != object2) {
+			passed++;
+		}
+		else {
+			errorPrinter << nameOfTest << ": Failure in test #" << total << ": " << object1 << " should not equal " << object2 << "!" << endl;
+			failed++;
+		}
+
+		return *this;
+	}
+
+	template<typename Fun, typename T> TestCase& check_function(Fun func, const T object1, const int object2) {
+
+		total++;
+		int res = (*func)(object1);
+
+		if (res == object2) {
+			passed++;
+		}
+		else {
+			errorPrinter << nameOfTest << ": Failure in test #" << total << ": Function should return " << object2 << " but returned " << res << "!" << endl;
+			failed++;
+		}
+
+		return *this;
+	}
+
+
+	template <typename T> TestCase& check_output(T object1, string str) {
+
+		total++;
+		ostringstream ss;
+		ss << object1;
+
+		if (ss.str() == str) {
+			passed++;
+		}
+		else {
+			errorPrinter << nameOfTest << ": Failure in test #" << total << ": string value should be " << str << " but is " << ss.str() << "!" << endl;
+			failed++;
+		}
+
+		return *this;
+	}
 };
 #endif
