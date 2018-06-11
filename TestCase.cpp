83be4a3d94b7ea9b68dde45c5fca85635a40c76b
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-11 21:59:28 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -5,8 +5,11 @@ TestCase::TestCase(string name, ostream& cerr) : errorPrinter(cerr.rdbuf()) {
 	total = passed = failed = 0;
 }
 
-void TestCase::print() {
+
+//print well known to return void but returning the object will allow a further test cases.
+TestCase& TestCase::print() {
 
 	errorPrinter << nameOfTest << ": " << failed << " failed, " << passed << " passed, " << total << " total." << endl;
 	errorPrinter << "---" << endl;
+	return *this;
 }
